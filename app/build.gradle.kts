3bfa682a173c9a12ff71595987dff53edb4647c7
Optimus <94816671+optimusprime123x@users.noreply.github.com>
2026-09-12 14:19:31 +0000

    Bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.mausam.home"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
 
